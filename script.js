7c5f4e3491bedef03f71e9b73ec61a7592395eb3
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-11-24 01:29:07 +0530

    second update

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-window.addEventListener('mousemove', function(){
+let circle = document.querySelector('#circle');
 
-	
+window.addEventListener('mousemove', function(details){
+		let x = details.clientX;
+		let y = details.clientY;
+
+		// we'll be using this set timeout so as to make the cursor follower a after a little delay, it takes a function first and then takes the time after which it'll update
+		setTimeout(function(){
+				circle.style.top = `${y}px`;
+				circle.style.left = `${x}px`;
+				}, 50);
 })
