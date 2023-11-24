7c0ef63a3b0cc4d95ffc84a1e9efade14f674f01
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2023-11-24 16:32:04 +0530

    3rd, feat: added all the animations using gsap

--- script.js
+++ script.js
@@ -10,3 +10,69 @@ window.addEventListener('mousemove', function(details){
 				circle.style.left = `${x}px`;
 				}, 50);
 })
+
+
+// all the animation stuff
+
+const button = document.querySelector("#button");
+
+
+button.addEventListener('click', function(){
+	
+});
+
+
+var tl = gsap.timeline();
+
+// gsap working, using the gsap file you will get an object and as such we can add animations using gsap
+// initial to final form basis, so add from to to using .
+tl
+.from('#wrapper',{ // you need to apply the wrapper so as to make the h1 show
+	duration: 3, // and it'll take 2s to come to its original size
+	scale: .7, // and it'll be 70% of its original size, 30% small than orignal
+	// which flow you want the animation to go
+	ease: 'Expo.easeInOut',
+	opacity: 0, // this means that it will not be visible since we are using the from with object
+})
+.from('#whitestrip', {
+	duration: 1.9,
+	width: 0,
+	ease: 'Expo.easeInOut',
+}, '-=2.5')
+.from('#blackcard', {
+	duration: 0.5,
+	x: 50,
+	opacity: 0,
+	ease: 'Expo.easeInOut',
+}, '-=1')
+.from('#linelem', {
+	duration: 0.5,
+	x: 50,
+	opacity: 0,
+	ease: 'Expo.easeInOut',
+}, '-=1')
+.from('#linelem .line', {
+	duration: 2,
+	width: 0,
+	opacity: 0,
+	ease: 'Expo.easeInOut',
+}, '-=1')
+.from('#blackcard p', {
+	duration: 1.5,
+	y: 30,
+	opacity: 0,
+	ease: 'Expo.easeInOut',
+}, '-=2')
+.from('#sidelem', {
+	duration: 2.5,
+	x: 30,
+	opacity: 0,
+	ease: 'Expo.easeInOut',
+}, '-=2')
+
+// when you want to apply animations one after another then you'll need to apply something called 'timeline'
+// gsap.from('#whitestrip', {
+// 	duration: 1.5,
+// 	width: 0,
+// 	ease: 'expo.easeInOut',
+// })
